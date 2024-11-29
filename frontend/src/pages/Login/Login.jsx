11438793ee48192d288312bd6db0642a73d7b38e
Harsh Rastogi <hharshrastogi@gmail.com>
2024-11-29 23:13:54 +0530

    Login page and Signup page added

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex flex-col items_center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg 
+        backdrop-opacity-0'>
+          <h1 className='text-3xl font-semibold text-center text-gray-300'> Login 
+            <span className='text-blue-500'> ChYapp </span>
+          </h1>
+
+          <form>
+            <div>
+
+                  <label className='label p-2'>
+                    <span className='text-base label-text text-blue-500 font-bold'>Username</span>
+                  </label>
+                  <input
+                    type='text'
+                    placeholder='Enter username'
+                    className='w-full input input-bordered h-10'
+						/>
+				
+            </div>
+
+            <div>
+
+            <label className='label'>
+							<span className='text-base label-text text-blue-500 font-bold'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+						/>
+            </div>
+
+            <a href='#' className='text-sm text-blue-500 font-black hover:underline hover:text-blue-600 mt-2 inline-block'>
+						{"Don't"} have an account?
+					</a>
+
+          <div>
+						<button className='btn btn-block btn-sm mt-2'> Login
+						</button>
+					</div>
+            
+          </form>
+        </div>
+    </div>
   )
 }
 
