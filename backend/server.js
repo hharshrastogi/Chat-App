d3db627df5e3bd52a691e5dc582e9efca95f1bf3
Harsh Rastogi <hharshrastogi@gmail.com>
2024-11-29 08:45:21 +0530

    Signup functionality

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDb from "./DB/connectToMongoDb.js"
 const app = express()
-
-dotenv.config()
 const PORT = process.env.PORT || 5000;
 
+dotenv.config()
 
-app.get('/',(req,res)=>{
-    res.send("server is ready")
-})
 
+app.use(express.json()) // tp parse incoming requests with JSON payloads (from req.body)
 app.use("/api/auth",authRoutes)
-app.listen(PORT,()=>console.log(`server is running  on ${PORT}`))
+
+// app.get('/',(req,res)=>{
+//     res.send("server is ready")
+// })
+
+app.listen(PORT,()=>{
+    connectToMongoDb()
+    console.log(`server is running  on ${PORT}`)
+})
 
