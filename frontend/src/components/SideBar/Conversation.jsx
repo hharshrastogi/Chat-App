48507d83025f2bc9396e82399a15631497707b3c
Harsh Rastogi <hharshrastogi@gmail.com>
2024-12-13 13:21:26 +0530

    GetConversations succesfull added

--- frontend/src/components/SideBar/Conversation.jsx
+++ frontend/src/components/SideBar/Conversation.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 
-const Conversation = () => {
+const Conversation = ({conversation , lastIdx,emoji}) => {
   return (
   <>
-    <div className='flex gap-2 items-center hover:bg-sky-200 rounded p-2 py-1 cursor-pointer'>
+    <div className='flex gap-2 items-center hover:bg-sky-200 border-gray-500 rounded-full p-2 py-1 cursor-pointer'>
         <div className='avatar online'>
             <div className='w-12 rounded-full'>
-                <img src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+                <img src={conversation.profilepic}
                  alt='user avatar'/>
             </div>
         </div>
-    </div>
+    
     <div>
         <div className='flex flex-col felx-1'>
             <div className='felx gap-3 justify-between'>
-                <p className='font-bold text-blue-500'>Jonh Doe</p>
-                    <span className='text-xl'></span>
+                <p className='font-bold text-blue-500'>{conversation.fullName}</p>
+                    <span className='text-xl'>{emoji}</span>
                 
             </div>
         </div>
     </div>
+    </div>
 
-    <div className='divider my-0 py-0 h-1 w-3'></div>
+   { !lastIdx && <div className='divider divider-accent my-0 py-1 h-1'/>}
   </>
   )
 }
--- frontend/src/components/SideBar/Conversations.jsx
+++ frontend/src/components/SideBar/Conversations.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import Conversation from './Conversation'
-import useGetConversations from '../../hooks/useGetConversations'
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
 
 const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
-  const {loading,conversations} = useGetConversations()
-  console.log("CONVERSATIONS:", conversations)
-  return (
-    <div className='py-2 felx flex-col overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
 
-      
-
-
-    </div>
-  )
-}
-
-export default Conversations
